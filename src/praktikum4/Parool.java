facb48e9ba63e823f6b717281ff84864f7bdaf0c
Karmen Lillemets <kalillem@klass2-18-l.intra.itcollege.ee>
2016-09-09 11:27:44 +0300

    praktikumi lõpp

--- src/praktikum4/Parool.java
+++ src/praktikum4/Parool.java
@@ -1,5 +1,23 @@
 package praktikum4;
 
-public class Parool {
+import praktikum1.TextIO;
 
+public class Parool {
+	public static void main(String[] args) {
+		System.out.println("Sisesta parool");
+		String oigeparool = "Salakala";
+		
+		while (true) {
+			String sisestatudParool = TextIO.getlnString();
+			
+			if (oigeparool.equals(sisestatudParool)) {
+				System.out.println("Õige parool");
+				break;
+			} else {
+				System.out.println("Vale parool");
+			}
+		}
+
+		
+	}
 }
--- src/praktikum4/Tehisintellekt.java
+++ src/praktikum4/Tehisintellekt.java
@@ -6,8 +6,13 @@ public class Tehisintellekt {
 	public static void main(String[] args) {
 		System.out.println("Palun sisesta kaks vanust");
 	int vanus1 = TextIO.getlnInt();
+	if (vanus1 < 0) {
+		System.out.println("Vanus ei saa olla negatiivne");
+	}
 	int vanus2 = TextIO.getlnInt();
-	
+	if (vanus2 < 0) {
+		System.out.println("Vanus ei saa olla negatiivne");
+	}
 	int vanustevahe = Math.abs(vanus1-vanus2);
 	
 	if (vanustevahe > 10) {
@@ -15,7 +20,7 @@ public class Tehisintellekt {
 	} else if (vanustevahe > 5) {
 		System.out.println("No ei sobi");
 	} else   {
-		System.out.println(" Sobib");
+		System.out.println("Sobib");
 	}
 		
 	}

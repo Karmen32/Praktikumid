8690beeacf16b6b237a338e1fdc47ddc911e5e94
Karmen Lillemets <kalillem@klass2-18-l.intra.itcollege.ee>
2016-09-09 10:58:19 +0300

    valmis kood

--- src/praktikum4/CumLaude.java
+++ src/praktikum4/CumLaude.java
@@ -4,18 +4,28 @@ import lib.TextIO;
 
 public class CumLaude {
 	public static void main(String[] args) {
-		System.out.println("Mis on sinu kaalutud keskhinne?");
+		System.out.println("Mis on sinu kaalutud keskmine?");
 
-		int keskhinne;
+		double keskhinne;
 		int lpuhinne;
-		
-		keskhinne = (int) TextIO.getlnDouble();
+		keskhinne = (double) TextIO.getlnDouble();
+		if (keskhinne > 5 || keskhinne < 0) {
+			System.out.println("Vigane hindesisestus");
+			return;
+		}
 		System.out.println("Mis on sinu lõputöö hinne?");
-
-		lpuhinne = (int) TextIO.getlnDouble();
+		lpuhinne = (int) TextIO.getlnInt();
+		if (lpuhinne > 5 || lpuhinne < 0) {
+			System.out.println("Vigane hindesisestus");
+			return;
+		}
 		
-if (keskhinne > 4.5 && lpuhinne == 5){
-		System.out.println("Jah, saad cum laude diplomile!");
-}	
-}
+		if (keskhinne > 4.5 && keskhinne <= 5 && lpuhinne == 5) {
+			System.out.println("Jah, saad cum laude diplomile!");
+		} else if (keskhinne < 4.5 || lpuhinne < 5 && lpuhinne >0) {
+			System.out.println("Ei saa!");
+		} else {
+			System.out.println("Vigane hindesisestus");
+		}
+	}
 }

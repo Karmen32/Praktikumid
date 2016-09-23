33393c50fad6ba02f8d781db15107d34cdb65253
Karmen Lillemets <kalillem@klass1-09-l.intra.itcollege.ee>
2016-09-23 12:42:29 +0300

    viimistlus

--- src/praktikum5/Tabel.java
+++ src/praktikum5/Tabel.java
@@ -2,7 +2,7 @@ package praktikum5;
 
 public class Tabel {
 	public static void main(String[] args) {
-		int tabeliSuurus = 7;
+		int tabeliSuurus = 9;
 		for (int i = 0; i < tabeliSuurus; i++) {
 			for (int j = 0; j < tabeliSuurus; j++) 
 				if (j == i){

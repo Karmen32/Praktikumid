e10c397b99d0d08e120aa443331e308274cb4744
Karmen Lillemets <Karmen32@msn.com>
2016-10-07 10:45:46 +0300

    exercise 1

---
+++ src/praktikum8/Arvud.java
@@ -0,0 +1,18 @@
+package praktikum8;
+
+import lib.TextIO;
+
+public class Arvud {
+	public static void main(String[] args) {
+		System.out.println("Anda 10 suvalist arvu.");
+		int[] arvud = new int[10];
+
+		for (int i = 0; i < arvud.length; i++) {
+			System.out.println("Palun sisesta arv " + i);
+			arvud[i] = TextIO.getlnInt();
+		}
+		for (int i= arvud.length -1; i >=0; i--)
+			System.out.println(arvud [i]);
+	}
+	
+}
